3c3c0d4f03e791449be413584b8e229306da1048
salar <salarnasiri74@gmail.com>
2016-08-07 06:24:19 +0430

    calss model updated

---
+++ project/models/class.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+
+from project.extensions import db
+
+class Class(db.Model):
+    __tablename__ = 'classes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Unicode, nullable=False)
+    body = db.Column(db.UnicodeText, nullable=False)
+    start_date = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    end_date = db.Column(db.DateTime, nullable=False)
+    price = db.Column(db.Integer, nullable=False, default=0)
